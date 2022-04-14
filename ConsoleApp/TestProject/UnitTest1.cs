c5828b95c98774a0add7376ffcce2e4efecf7c66
zulhamzakaria <m.zulham.zakaria@gmail.com>
2022-04-14 12:34:20 +0800

    First Push

---
+++ ConsoleApp/TestProject/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using(var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ConsoleApp.Program.Main();
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
